30cb5c76527169f29acb16d6c89a5faf82e02f31
Olivia Cubela <oliviacubela@gmail.com>
2020-06-04 17:22:50 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,10 @@
 def oxford_comma(array)
-  case array.length 
+  case array.length
 end 	when 1
   "#{array[0]}"
 when 2
   array[0..1].join(" and ")
-else 
+else
   array[0...-1].join(", ") << ", and #{array[-1]}"
-end 
-end 
+end
+end
