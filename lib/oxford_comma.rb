ececaad06e8df38f4d1b0dc4b3764c77faad11ad
Olivia Cubela <oliviacubela@gmail.com>
2020-06-04 16:51:55 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,5 @@
 def oxford_comma(array)
 array.join
 array.join(" and ")
-implode(',', array_slice($array, 0, -1)) . ' and ' . array_slice($array, -1)[0]
+array.implode(',', array_slice($array, 0, -1)).' and '.array_slice($array, -1)[0]
 end
