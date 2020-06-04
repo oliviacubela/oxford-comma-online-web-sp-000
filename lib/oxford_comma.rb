29e7c5dfcece2fc5345c99d473f8b5917e2ef867
Olivia Cubela <oliviacubela@gmail.com>
2020-06-04 17:22:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,10 @@
 def oxford_comma(array)
-array.join
-array.join(" and ")
-end
+  case array.length 
+end 	when 1
+  "#{array[0]}"
+when 2
+  array[0..1].join(" and ")
+else 
+  array[0...-1].join(", ") << ", and #{array[-1]}"
+end 
+end 
